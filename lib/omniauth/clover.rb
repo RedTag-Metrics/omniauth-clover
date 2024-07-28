a8121c4c1a3498a540e39cca372c6bd2a999dd80
Matt McFarling <matthew.mcfarling@gmail.com>
2024-07-28 19:06:46 -0400

    initial commit

---
+++ lib/omniauth/clover.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "clover/version"
+
+module Omniauth
+  module Clover
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/clover_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::Clover do
+  it "has a version number" do
+    expect(Omniauth::Clover::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
